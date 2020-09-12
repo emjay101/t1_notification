c882711dc755e960f1c614ef2328b8a0cf239478
emjay101 <14279774+emjay101@users.noreply.github.com>
2020-09-12 07:49:27 +0200

    init

---
+++ t1_notification.py
@@ -0,0 +1,55 @@
+﻿from threading import Thread
+from time import sleep as ResidentSleeper
+from tribes import TribesMasterClient
+from tooltip import WindowsBalloonTip
+from sys import exc_info
+from traceback import format_exc
+import os
+
+if os.name == 'nt':
+    from  winsound import PlaySound, SND_FILENAME
+def snd(name):
+    if os.name == 'nt':
+        PlaySound("sound\\%s.wav" % name, SND_FILENAME)
+
+if __name__ == '__main__':
+    tool_tip = WindowsBalloonTip(icon="tribes.ico")
+
+    def checkTribesAll():
+        global tool_tip
+
+        lastcnt = {}
+
+        tribes_list = [
+            TribesMasterClient('67.222.138.46', 28006),
+            TribesMasterClient('52.188.16.233', 28001)
+        ]
+        while True:
+            os.system("cls")
+            for tribes_srv in tribes_list:
+                srvhash = "%s_%s" % (tribes_srv.ip, tribes_srv.port)
+                lastplayercnt = lastcnt.get(srvhash, 0)
+                try:
+                    tribes_srv.Query(readplayerdata=False)
+                    if  tribes_srv.playerCount >= 3 and lastplayercnt < 3 :
+                        snd("ir_end")
+                        tool_tip.show("%d/%d" % (tribes_srv.playerCount, tribes_srv.maxPlayers), tribes_srv.serverName.decode(), 5)
+                    lastcnt[srvhash] = tribes_srv.playerCount
+                except:
+                    print(srvhash)
+                    print(str(format_exc()))
+                    print(str(exc_info()[0]))
+            ResidentSleeper(120)
+
+
+    def starttasks():
+        ___tlist = [
+            #Thread(target=webserv),
+            #Thread(target=ffmpegWatcher),
+            Thread(target=checkTribesAll),
+            ]
+        for ___t in ___tlist:
+            ___t.start()
+        for ___t in ___tlist:
+            ___t.join()
+    starttasks()
---
+++ tooltip.py
@@ -0,0 +1,48 @@
+﻿from win32api import *
+from win32gui import *
+import win32con
+import sys, os
+import struct
+import time
+
+
+class WindowsBalloonTip:
+    classAtom = None
+
+    def __init__(self, *args, **kwargs):
+        message_map = {
+                win32con.WM_DESTROY: self.OnDestroy,
+        }
+
+        # Register the Window class.
+        self.hinst = GetModuleHandle(None)
+        if not WindowsBalloonTip.classAtom:
+            wc = WNDCLASS()
+            wc.hInstance =  self.hinst
+            wc.lpszClassName = "PythonTaskbar"
+            wc.lpfnWndProc = message_map # could also specify a wndproc.
+            WindowsBalloonTip.classAtom = RegisterClass(wc)
+
+
+        iconPathName = os.path.abspath(os.path.join( sys.path[0], kwargs.get("icon", "balloontip.ico") ))
+        icon_flags = win32con.LR_LOADFROMFILE | win32con.LR_DEFAULTSIZE
+        try:
+           self.hicon = LoadImage(self.hinst, iconPathName,   win32con.IMAGE_ICON, 0, 0, icon_flags)
+        except:
+           self.hicon = LoadIcon(0, win32con.IDI_APPLICATION)
+
+    def show(self,title, msg, duration=10):
+        style = win32con.WS_OVERLAPPED | win32con.WS_SYSMENU
+        self.hwnd = CreateWindow( WindowsBalloonTip.classAtom, "Taskbar", style,  0, 0, win32con.CW_USEDEFAULT, win32con.CW_USEDEFAULT,  0, 0, self.hinst, None)
+        flags = NIF_ICON | NIF_MESSAGE | NIF_TIP
+        nid = (self.hwnd, 0, flags, win32con.WM_USER+20, self.hicon, "tooltip")
+        Shell_NotifyIcon(NIM_ADD, nid)
+        Shell_NotifyIcon(NIM_MODIFY, (self.hwnd, 0, NIF_INFO, win32con.WM_USER+20,  self.hicon, "Balloon  tooltip",title,200,msg))
+        time.sleep(duration)
+        Shell_NotifyIcon(NIM_DELETE, nid)
+        ShowWindow(self.hwnd, win32con.SW_HIDE)
+
+    def OnDestroy(self, hwnd, msg, wparam, lparam):
+        nid = (self.hwnd, 0)
+        Shell_NotifyIcon(NIM_DELETE, nid)
+        PostQuitMessage(0)
